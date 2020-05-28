81272c8fa4706a5dc7f71ed47df0b30349817b2c
Scott Winges <30635282+swinges@users.noreply.github.com>
2020-05-28 09:17:57 -0700

    Update siteconfig.js
    More initializing

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,15 +23,15 @@ const users = [
 const siteConfig = {
   title: 'Kalm', // Title for your website.
   tagline: 'A kalm an easy interface for running kubernetes',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://swinges.github.io/', // Your website URL
+  baseUrl: '/kapp_sass/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'kapp_sass',
+  organizationName: 'swinges',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
@@ -39,18 +39,19 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
-    {page: 'help', label: 'Help'},
-    {blog: true, label: 'Blog'},
+    // {doc: 'doc4', label: 'API'},
+    // {page: 'help', label: 'Help'},
+    {blog: true, label: 'Blog'}
+    {href: "https://github.com/", label: "Login"},
   ],
 
   // If you have users set above, you add it here:
   users,
 
   /* path to images for header/footer */
-  headerIcon: 'img/favicon.ico',
-  footerIcon: 'img/favicon.ico',
-  favicon: 'img/favicon.ico',
+  headerIcon: 'img/Kubernetes_logo_without_workmark.svg',
+  footerIcon: 'img/Kubernetes_logo_without_workmark.svg',
+  favicon: 'img/Kubernetes_logo_without_workmark.svg',
 
   /* Colors for website */
   colors: {
@@ -73,7 +74,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} Random Block Labs, Inc.`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
